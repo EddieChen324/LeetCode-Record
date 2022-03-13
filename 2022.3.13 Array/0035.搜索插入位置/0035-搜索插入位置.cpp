71443c5006f68e5523811a957c3575f12f8390ef
EddieChen324 <70046892+EddieChen324@users.noreply.github.com>
2022-03-13 17:51:13 +0800

    Update 0035-搜索插入位置.cpp

--- 2022.3.13 Array/0035.搜索插入位置/0035-搜索插入位置.cpp
+++ 2022.3.13 Array/0035.搜索插入位置/0035-搜索插入位置.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         int left = 0;
-        int right = nums.size() - 1;
+        int right = nums.size() - 1;    //二分查找法
         // if (nums[0] > target) {
         //     return 0;
         // }
